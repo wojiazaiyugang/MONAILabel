cfa5d32b0e168054a8e28026ccb8c64b3f14bf95
wojiazaiyugang <wojiazaiyugang@outlook.com>
2022-09-19 11:37:27 +0800

    update swin_unetr_click_tooth_segmentation

--- apps/radiology/lib/infers/swin_unetr_click_tooth_segmentation.py
+++ apps/radiology/lib/infers/swin_unetr_click_tooth_segmentation.py
@@ -1,14 +1,11 @@
-import random
 from pathlib import Path
 from typing import Sequence, Callable, Union, Dict, Tuple, Any, List
 
-import torch
-from infer import swin_unetr_click_tooth_segmentater, BBox3D, DetectResult3D, Image3D, Point3D, \
-    swin_unetr_click_tooth_segmentater_025
+from infer import Image3D, Point3D, click_tooth_segmentation_swin_unetr
 
 from monailabel.interfaces.tasks.infer import InferTask
 
-model = swin_unetr_click_tooth_segmentater_025
+model = click_tooth_segmentation_swin_unetr
 
 
 class SwinUnetrClickToothSegmentation(InferTask):
@@ -32,20 +29,12 @@ class SwinUnetrClickToothSegmentation(InferTask):
         """
         image = Image3D.from_path(Path(request["image"]))
         click: List[int] = request["foreground"][-1]
-        image_spacing = image.spacing
-        model_spacing = model.model_config.spacing
-        ratio = [image_spacing[0] / model_spacing[0], image_spacing[1] / model_spacing[1], image_spacing[2] / model_spacing[2]]
-        center = Point3D(x=click[0] * ratio[0], y=click[1] * ratio[1], z=click[2] * ratio[2]).to_int()
-        point1 = center.offset(-model.model_config.image_size[0] // 2,
-                               -model.model_config.image_size[1] // 2,
-                               -model.model_config.image_size[2] // 2)
-        point7 = center.offset(model.model_config.image_size[0] // 2,
-                               model.model_config.image_size[1] // 2,
-                               model.model_config.image_size[2] // 2)
-        result = model.infer(image=image,
-                             detect_result=DetectResult3D(bbox=BBox3D(point1=point1,
-                                                                      point7=point7)))
-        seg = Image3D(torch.argmax(result, dim=0))
+        center = Point3D(x=click[0], y=click[1], z=click[2]).to_int()
+        result = model.infer(image=image, click_point=center)
+        seg = Image3D(result[1])
+        seg.re_spacing(spacing=image.spacing, mode="bilinear")
+        seg.gaussian_smooth(sigma=1)
+        seg.as_discrete(threshold=0.5)
         output_file = Path("/tmp/seg.nii.gz")
         seg.save(output_file)
         return str(output_file), {"label_names": self.labels}
